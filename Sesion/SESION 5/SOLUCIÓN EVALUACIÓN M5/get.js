b453153f4049da9d9b9ec8352d7acab6458da632
Ildik0-0 <iremar.rivas@gmail.com>
2024-08-26 20:49:15 -0400

    TEST

--- Sesion/SESION 5/SOLUCIÓN EVALUACIÓN M5/get.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("form").addEventListener("submit",  (event) => {
-    event.preventDefault();
-    let userName = document.getElementById('input').value;
-   
-    console.log(userName);
-});
-
--- Sesion/SESSION 9/client/src/index.js
+++
@@ -1,3 +0,0 @@
-function mostrarMensaje(){
-    document.getElementById('caja2').style.display = "block";
-    }
