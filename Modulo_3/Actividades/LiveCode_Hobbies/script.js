fe033caed15049d716ec6bbc8cf99ed0f1591b58
Ildik0-0 <iremar.rivas@gmail.com>
2024-08-29 17:10:31 -0400

    Buscar Hobbie JS

---
+++ Modulo_3/Actividades/LiveCode_Hobbies/script.js
@@ -0,0 +1,30 @@
+// Obtener referencias a los elementos HTML
+const inputHobby = document.getElementById('hobby');
+const listaHobbies = document.getElementById('listaHobbies');
+const inputBuscar = document.getElementById('buscarHobby');
+
+// Función para agregar un hobby a la lista
+function agregarHobby() {
+  const nuevoHobby = inputHobby.value;
+  if (nuevoHobby !== '') {
+    const li = document.createElement('li');
+    li.textContent = nuevoHobby;
+    listaHobbies.appendChild(li);
+    inputHobby.value = '';
+  }
+}
+
+// Función para buscar un hobby en la lista (implementarás esta función)
+function buscarHobby() {
+    const hobbyABuscar = inputBuscar.value.toLowerCase(); // Convertimos a minúsculas para una comparación más robusta
+    const listaHobbiesArray = Array.from(listaHobbies.children); // Convertimos la lista de elementos li en un array
+  
+    listaHobbiesArray.forEach(li => {
+      if (li.textContent.toLowerCase().includes(hobbyABuscar)) {
+        li.style.backgroundColor = 'yellow'; // Resaltamos el elemento encontrado
+      } else {
+        li.style.backgroundColor = 'white';
+      }
+    });
+  }
+  
